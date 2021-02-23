8c3214006eb9bfbf541941600ba7791df072c3ca
Syd Sisco <hello@sydsis.co>
2021-02-23 12:03:33 -0800

    turn the counter red if the user has typed too many characters

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -9,6 +9,14 @@ $(document).ready(function() {
     // find the element that shows the character count
     const counterElement = $(this).siblings('div').children('output');
 
+    // turn the counter red if the user has types too much
+    // do this by adding the class 'toolong'
+    if (remainingChars < 0) {
+      $(counterElement).addClass('toolong');
+    } else {
+      $(counterElement).removeClass('toolong');
+    }
+
     $(counterElement).text(remainingChars);
   });
 });
